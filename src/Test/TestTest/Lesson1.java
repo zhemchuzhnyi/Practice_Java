5ba11bc93508b5c64764587aa00fb4c887257fa1
Andrey_O <a_reshkin@inbox.ru>
2025-02-04 22:41:26 +0300

    Hello World)

--- src/Test/TestTest/Lesson1.java
+++ src/Test/TestTest/Lesson1.java
@@ -2,8 +2,10 @@ package TestTest;
 
 public class Lesson1 {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, i have 30 years old)");
 
     }
 }
