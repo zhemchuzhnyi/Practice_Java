83d356c0d2cca189a4e0dd374254fd97927b9978
Andrey_O <a_reshkin@inbox.ru>
2025-02-05 22:02:39 +0300

    Rubai Omar Kxayam

---
+++ src/Test/TestTest/Rubai.java
@@ -0,0 +1,32 @@
+package TestTest;
+
+public class Rubai {
+    public static void main(String[] args) {
+
+//        System.out.println("Welcome to the first lesson!!!");
+//        System.out.print("Hello...");
+//
+//System.out.print("Меня зовут ");
+//System.out.println("- Андрей)");
+
+//System.out.print("A - ");
+//System.out.println("a");
+//System.out.print("B - ");
+//System.out.println("b");
+//System.out.print("C - ");
+//System.out.println("c");
+
+//System.out.println("abcdef");
+//System.out.println();
+//System.out.println("ghjklm");
+
+                System.out.println("                Рубаи");
+                System.out.println("Много лет размышлял я над жизнью земной.");
+                System.out.println("Непонятного нет для меня под луной.");
+                System.out.println("Мне известно, что мне ничего не известно!");
+                System.out.println("Вот последняя правда, открытая мной.");
+                System.out.println("                                О.Хайям");
+
+            }
+        }
+
