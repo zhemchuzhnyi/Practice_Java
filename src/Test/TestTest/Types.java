9d460e208d20e386da5b97dc69e4a97690c406c0
Andrey_O <a_reshkin@inbox.ru>
2025-02-06 16:19:01 +0300

    Types

---
+++ src/Test/TestTest/Types.java
@@ -0,0 +1,24 @@
+package TestTest;
+
+public class Types {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 0;
+
+        //+= , -= / *= , /=
+        // a = a + 3
+        // a += 3
+
+        // a+=10;
+        // b-=2;
+        // b*=2;
+        //b/=2;
+
+        //a = b = c = 18; - все значения будут равны 18
+        //код читается справа на лево
+
+        System.out.println();
+
+    }
+}
