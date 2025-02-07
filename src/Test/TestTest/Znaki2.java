08de7d1abe72c70b460409ed569e098f95e089a5
Andrey_O <a_reshkin@inbox.ru>
2025-02-07 16:36:37 +0300

    Types2

---
+++ src/Test/TestTest/Znaki2.java
@@ -0,0 +1,76 @@
+package TestTest;
+
+public class Znaki2 {
+    public static void main(String[] args) {
+//        int a = 5;
+//        int b = 11;
+//
+//        int c = a + b;
+//
+//        System.out.println(c);
+//
+//        int w = 30;
+//        int x = 16;
+//
+//        int y = w - x;
+//
+//        System.out.println(y);
+//
+//        int e = 5;
+//        int r = 5;
+//
+//        int t = e * r;
+//
+//        System.out.println(t);
+//
+//        int p = 50;
+//        int o = 10;
+//
+//        int z = p / o;
+//
+//        System.out.println(z);
+//
+//
+//        int v = 10;
+//        int n = 3;
+//
+//        int m = v / n; // деление инта на инт - это инт
+//
+//        System.out.println(m);
+//
+//
+//        double k = 11;
+//        double l = 3;
+//
+//        double j = k / l; // дробные числа, запятая есть в ответе
+//
+//        System.out.println(j);
+
+
+
+//        int a = 11;
+//        int b = 3;
+//
+//        int celaya = a / b;
+//        int ostatok = a % b; // % - показывает остаток от деления
+//
+//        System.out.println(celaya);
+//        System.out.println(ostatok);
+
+
+
+                //++ - к какому-то значению прибавляем +1
+                //-- - от какого-то числа отнимаем -1
+                // может стоять вначале (префикс), в конце(постфикс)
+
+                int x = 5;
+                int y = 3;
+                int z = x - y++;
+
+                System.out.println(z);
+
+
+
+
+            }
+        }
